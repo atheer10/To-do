6898753a98ae68e6b92a3ee7c31f0ac994a2315b
atheer10 <atheer.m.070@gmail.com>
2020-07-28 23:01:36 +0300

    second

--- js/main.js
+++ js/main.js
@@ -10,32 +10,3 @@ $(document).ready(function () {
     $("#menu-on").hide();
   });
 });
-
-//   $("#tasks-menu").click(function () {
-//     $(".tasklist").show();
-//     $(".coming").hide();
-//   });
-
-//   $("ul li").click(function () {
-//     $("li").removeClass("active");
-//     $(this).addClass("active");
-//   });
-// });
-
-// if ($("ul li").hasClass(".active") {
-//     // Do something if class exists
-// } else {
-//     // Do something if class does not exist
-// }
-
-// if ($(".mydivclass")[0]) {
-//     // Do something if class exists
-// } else {
-//     // Do something if class does not exist
-// }
-
-// if ($("#tasks-menu").hasClass("active")) {
-//   //   $(".tasklist").hide();
-//   $("p").hide();
-// } else {
-//   $(".coming").hide();
